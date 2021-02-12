5955f818cfb992b8371e9d4c6704621dc68345fe
Douglas Paschoal <douglaspaschoal080@gmail.com>
2021-02-12 15:07:01 -0300

    ESTILIZANDO A PÁGINA INICIAL

--- themes/home/home.php
+++ themes/home/home.php
@@ -1,9 +1,12 @@
-<?php 
+<?php include __DIR__.'./../header/header.php';?>
 
-include __DIR__.'./../header/header.php';
+<div class="header-home">    
+    <h1 class="header-home-title">
+        Para quem tem fome de boas lembranças!
+    </h1>
 
+    <h2 class="header-home-title" id="header-title-small"> Conheça nossos produtos!</h2>
+</div>
 
+<?php include __DIR__.'./../footer/footer.php'; ?>
 
-include __DIR__.'./../footer/footer.php';
-
-?>
